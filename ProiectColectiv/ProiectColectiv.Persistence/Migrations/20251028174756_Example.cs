b1e66303292aa0de613eed0385c311e28eb1b842
Fanttatraieste <vladuta61@gmail.com>
2025-10-28 19:52:07 +0200

    added back example migration

--- ProiectColectiv/ProiectColectiv.Persistence/Migrations/20251028174756_Example.cs
+++ ProiectColectiv/ProiectColectiv.Persistence/Migrations/20251028174756_Example.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 namespace ProiectColectiv.Persistence.Migrations
 {
     /// <inheritdoc />
-    public partial class Examples : Migration
+    public partial class Example : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
