cf7069e31b9c3400500d6639795c2802c288165a
PrialaRadu <priala.radu@gmail.com>
2025-11-04 12:45:04 +0200

    Created Specialization and Subject tables

---
+++ ProiectColectiv/ProiectColectiv.Application/Models/Subject.cs
@@ -0,0 +1,17 @@
+﻿using ProiectColectiv.Application.Models;
+using ProiectColectiv.Domain.Enums;
+
+namespace ProiectColectiv.Application.Models
+{
+    public class Subject : BaseModel
+    {
+        public string Name { get; set; }
+        public int Credits { get; set; }
+        public MajorEnum Major { get; set; }
+
+        public SubjectTypeEnum SubjectType { get; set; }
+
+        // relatie many to many cu specialization
+        public ICollection<Specialization> Specializations { get; set; }
+    }
+}
---
+++ ProiectColectiv/ProiectColectiv.Domain/Enums/MajorEnum.cs
@@ -0,0 +1,8 @@
+﻿namespace ProiectColectiv.Domain.Enums
+{
+    public enum MajorEnum
+    {
+        Math = 1,
+        ComputerScience = 2
+    }
+}
